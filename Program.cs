15e51fffde7cbc361fb94879c2e065b19f5d1082
zominym <marc.zominy@etu.univ-lyon1.fr>
2016-03-11 11:22:22 +0100

    added data files

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace LieuxDeFormation
 {
@@ -6,7 +9,15 @@ namespace LieuxDeFormation
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			FileStream fs = File.Open ("../../ListeAgences_100.txt", FileMode.Open);
+
+			UTF8Encoding temp = new UTF8Encoding(true);
+			byte[] b = new byte[1024];
+
+			while (fs.Read(b,0,b.Length) > 0)
+			{
+				Console.WriteLine(temp.GetString(b));
+			}
 		}
 	}
 }
