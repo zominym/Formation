57745a727a6e72514af63c47af1c259981fbe0d1
Vil <vilghost@gmail.com>
2016-04-01 09:43:33 +0200

    Neighborhood builder created

--- Model/Metaheuristic/Solution.cs
+++ Model/Metaheuristic/Solution.cs
@@ -7,28 +7,52 @@ namespace Metaheuristic
     public class Solution
     {
 
-        private Dictionary<Agency, City> _value;
-        private List<Solution> _neighbors;
+        private Agency[] _agencies;
+        private City[] _cities;
+        private List<Solution> _neighbors = null;
 
-        public Solution(Dictionary<Agency, City> value)
-        {
-            _value = value;
-              
-        }
-
-        public Dictionary<Agency, City> Value
-        {
-            get { return _value; }
+        public Solution(Agency[] agencies, City[] cities){
+            _agencies = agencies;
+            _cities = cities;
         }
 
         public List<Solution> Neighbors
         {
-            get { return _neighbors; }
+            get { 
+                if (_neighbors == null)
+                    buildNeighborhood();
+                return _neighbors;
+            }
         }
 
+        public Agency[] Agencies
+        {
+            get { return _agencies; }
+        }
+
+        public City Cities
+        {
+            get { return _cities; }
+        }
+            
         private void buildNeighborhood()
         {
-            //TODO
+            _neighbors = new List<Solution>();
+            for (int i = 0; i < Agencies.Length; i++)
+            {
+                for (int j = i + 1; j < Agencies.Length; j++) 
+                {
+                    Solution tmp = new Solution(Agencies, Cities);
+                    tmp.swap(i, j);
+                    _neighbors.Add(tmp);
+                }
+            }
+        }
+
+        private void swap(int a, int b){
+            City tmp = this.Cities[a];
+            this.Cities[a] = this.Cities[b];
+            this.Cities[b] = this.Cities[a];
         }
     }
 }
